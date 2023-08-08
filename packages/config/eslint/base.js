1fae3a60fae4f732b734325bcc4a225ca9b2843c
Vu Nguyen <shardphoneix@gmail.com>
2023-08-08 08:55:30 -0700

    feat: eslint will use prettier config for formatting

--- packages/config/eslint/base.js
+++ packages/config/eslint/base.js
@@ -9,12 +9,19 @@ const config = {
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint", "import"],
+  plugins: ["@typescript-eslint", "import", "prettier"],
   rules: {
     "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
     "@typescript-eslint/consistent-type-imports": ["warn", { prefer: "type-imports", fixStyle: "separate-type-imports" }],
     "@typescript-eslint/no-misused-promises": [2, { checksVoidReturn: { attributes: false } }],
     "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+    "prettier/prettier": [
+      "error",
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
   },
   ignorePatterns: ["**/.eslintrc.cjs", "**/*.config.js", "**/*.config.cjs", "packages/config/**", ".next", "dist", "pnpm-lock.yaml"],
   reportUnusedDisableDirectives: true,
