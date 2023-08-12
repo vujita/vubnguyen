1ffdb47652c76e69c4e6b4cc751353ec621335c7
Vu Nguyen <shardphoneix@gmail.com>
2023-08-12 15:18:42 -0700

    chore default is light theme

--- apps/vubnguyen/src/components/theme-switcher.tsx
+++ apps/vubnguyen/src/components/theme-switcher.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect } from "react";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTheme } from "next-themes";
@@ -7,6 +8,11 @@ import { useTheme } from "next-themes";
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
   const isLightTheme = theme === "light";
+  useEffect(() => {
+    if (theme === "system") {
+      setTheme("light");
+    }
+  }, [theme, setTheme]);
 
   return <FontAwesomeIcon icon={isLightTheme ? faMoon : faSun} className="h-[32px] w-[32px] text-gray-400" onClick={() => setTheme(isLightTheme ? "dark" : "light")} />;
 };
