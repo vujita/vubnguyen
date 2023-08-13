ab1f7325684d62831edaa94f3e75c9832840aaa9
Vu Nguyen <shardphoneix@gmail.com>
2023-08-13 09:59:58 -0700

    fix: no render theme toggle untill on client (#30)
    Any questions should be directed to @vujita
    
    ---
    
    Replace any ":question:" below with information about your pull request.
    
    ## Pull Request Details
    
    Provide details about your pull request and what it adds, fixes, or
    changes.
    
    :question:
    
    ## Breaking Changes
    
    Describe what features are broken by this pull request and why, if any.
    
    :question:
    
    ## Issues Fixed
    
    Enter the issue numbers resolved by this pull request below, if any.
    
    1. :question:
    
    ## Other Relevant Information
    
    Provide any other important details below.
    
    :question:

--- apps/vubnguyen/src/components/theme-switcher.tsx
+++ apps/vubnguyen/src/components/theme-switcher.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTheme } from "next-themes";
@@ -8,11 +8,19 @@ import { useTheme } from "next-themes";
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
   const isLightTheme = theme === "light";
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
   useEffect(() => {
     if (theme === "system") {
       setTheme("light");
     }
   }, [theme, setTheme]);
+  // We won't be able to detect the users localStorage for theme
+  if (!mounted) {
+    return null;
+  }
 
   return <FontAwesomeIcon icon={isLightTheme ? faMoon : faSun} className="h-[32px] w-[32px] text-gray-400" onClick={() => setTheme(isLightTheme ? "dark" : "light")} />;
 };
