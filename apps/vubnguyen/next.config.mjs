80f18c6c28f4d4ffec7a37e252eb38abb6d41310
Vu Nguyen <shardphoneix@gmail.com>
2023-08-25 07:49:02 -0700

    feat: storybook

--- apps/vubnguyen/next.config.mjs
+++ apps/vubnguyen/next.config.mjs
@@ -2,13 +2,17 @@
 import "./src/env.mjs";
 import "@vujita/auth/env.mjs";
 
+/**
+ * @type {string[]}
+ */
+
 /** @type {import("next").NextConfig} */
 const config = {
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
   transpilePackages: ["@vujita/api", "@vujita/auth", "@vujita/db"],
-  /** We already do linting and typechecking as separate tasks in CI */
-  eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
 
