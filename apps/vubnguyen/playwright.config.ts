87225175039ac225496665fb21e6d6add72c3098
Vu Nguyen <shardphoneix@gmail.com>
2024-01-06 13:02:51 -0800

    feat: add dev preview
    Signed-off-by: Vu Nguyen <shardphoneix@gmail.com>

--- apps/vubnguyen/playwright.config.ts
+++ apps/vubnguyen/playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm run preview",
+    command: "pnpm run dev:preview",
     reuseExistingServer: !process.env.CI,
     url: "http://127.0.0.1:3000",
   },
