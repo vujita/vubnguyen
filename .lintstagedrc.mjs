7019629f0280d8397f4582dd877be133bc95d151
Vu Nguyen <shardphoneix@gmail.com>
2024-01-07 02:04:03 -0800

    chore: remove old storybook setup (#114)
    Any questions should be directed to @vujita
    
    ---
    
    Replace any ":question:" below with information about your pull request.
    
    ## Pull Request Details
    
    Provide details about your pull request and what it adds, fixes, or
    changes.
    
    :question:
    
    ## Breaking Changes
    
    Describe what features are broken by this pull request and why, if any.
    
    :question:
    
    ## Issues Fixed
    
    Enter the issue numbers resolved by this pull request below, if any.
    
    1. :question:
    
    ## Other Relevant Information
    
    Provide any other important details below.
    
    :question:
    
    ---------
    
    Signed-off-by: Vu Nguyen <shardphoneix@gmail.com>

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -3,6 +3,7 @@ export default {
   "pnpm-workspace.yaml": ["prettier --write", "git add"],
   "*.{css}": ["prettier --write", "git add"],
   "*.{mjs,json}": ["prettier --write", "git add"],
+  "*.{html}": ["prettier --write", "git add"],
   "*.{js,jsx,ts,tsx,cjs,mjs,md}": ["prettier --write --list-different", "git add"],
   "{apps,packages}/**/*.{js,jsx,ts,tsx,cjs,mjs,md}": ["prettier --write --list-different", "eslint --fix", "git add"],
 };
