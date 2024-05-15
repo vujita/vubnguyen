d1082c8a173709b47174c489ed468149e80ef396
Vu Nguyen <shardphoneix@gmail.com>
2024-05-15 09:45:31 -0700

    add eslint import resolver typescript

--- packages/config/eslint/base.js
+++ packages/config/eslint/base.js
@@ -4,6 +4,32 @@ const config = {
     es2022: true,
     node: true,
   },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    // "import/resolver": {
+    //   typescript: {
+    //     alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+    //
+    //     // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
+    //
+    //     // use <root>/path/to/folder/tsconfig.json
+    //     project: "path/to/folder",
+    //
+    //     // Multiple tsconfigs (Useful for monorepos)
+    //
+    //     // use a glob pattern
+    //     project: "packages/*/tsconfig.json",
+    //
+    //     // use an array
+    //     project: ["packages/module-a/tsconfig.json", "packages/module-b/tsconfig.json"],
+    //
+    //     // use an array of glob patterns
+    //     project: ["packages/*/tsconfig.json", "other-packages/*/tsconfig.json"],
+    //   },
+    // },
+  },
   extends: ["turbo", "eslint:recommended", "plugin:@typescript-eslint/recommended-type-checked", "plugin:@typescript-eslint/stylistic-type-checked", "prettier"],
   ignorePatterns: ["**/.eslintrc.cjs", "**/*.config.js", "**/*.config.cjs", "packages/config/**", ".next", "dist", "pnpm-lock.yaml"],
   parser: "@typescript-eslint/parser",
