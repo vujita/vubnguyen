21d713bef9816434783e87ddb0187ffb641fc9a3
Vu Nguyen <shardphoneix@gmail.com>
2024-05-14 22:44:11 -0700

    add popup zustand store (#212)
    scaffold out message
    
    Signed-off-by: Vu Nguyen <shardphoneix@gmail.com>
    
    update lint
    
    Any questions should be directed to @vujita
    
    ---
    
    Replace any ":question:" below with information about your pull request.
    
    ## Pull Request Details
    
    Provide details about your pull request and what it adds, fixes, or
    changes.
    
    :question:
    
    ## Breaking Changes
    
    Describe what features are broken by this pull request and why, if any.
    
    :question:
    
    ## Issues Fixed
    
    Enter the issue numbers resolved by this pull request below, if any.
    
    1. :question:
    
    ## Other Relevant Information
    
    Provide any other important details below.
    
    :question:

--- apps/newnew-performancetool/src/components/MemoryStats.tsx
+++ apps/newnew-performancetool/src/components/MemoryStats.tsx
@@ -1,3 +1,52 @@
-import { memoryStore } from "../stores/memory";
+import { type FC } from "react";
 
-console.log("memoryStats", memoryStore);
+import { MemorySnapshot, useMemoryStates } from "../stores/memory";
+
+const memoryStat = (memoryState: MemorySnapshot): number => ((memoryState.usedJSHeapSize / memoryState.jsHeapSizeLimit) * 100).toFixed(2);
+
+const MEM_STAT_WIDTH = 200;
+export const MemoryStats: FC = () => {
+  const memoryStates = useMemoryStates();
+  const last = memoryStates.at(-1);
+  return (
+    <div
+      style={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        padding: 5,
+        boxSizing: "border-box",
+      }}
+    >
+      {last && (
+        <div
+          style={{
+            margin: "auto",
+            width: "fit-content",
+          }}
+        >{`${memoryStat(last)}%`}</div>
+      )}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          backgroundColor: "lightgrey",
+          opacity: 0.8,
+          alignItems: "flex-end",
+          height: 100,
+          width: MEM_STAT_WIDTH,
+        }}
+      >
+        {memoryStates.map((memoryState) => (
+          <div
+            style={{
+              backgroundColor: "lightgreen",
+              width: MEM_STAT_WIDTH / memoryStates.length,
+              height: memoryStat(memoryState) + "%",
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
