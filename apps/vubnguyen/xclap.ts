cddc8b7c9093bfb6c33622715e8906b69bef6e1a
Vu Nguyen <shardphoneix@gmail.com>
2024-06-15 08:10:10 -0700

    Ci tasks (#243)
    Any questions should be directed to @vujita
    
    ---
    
    Replace any ":question:" below with information about your pull request.
    
    ## Pull Request Details
    
    Provide details about your pull request and what it adds, fixes, or
    changes.
    
    :question:
    
    ## Breaking Changes
    
    Describe what features are broken by this pull request and why, if any.
    
    :question:
    
    ## Issues Fixed
    
    Enter the issue numbers resolved by this pull request below, if any.
    
    1. :question:
    
    ## Other Relevant Information
    
    Provide any other important details below.
    
    :question:

---
+++ apps/vubnguyen/xclap.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+const xrun = require("@xarc/run");
+const rimraf = require("rimraf");
+
+xrun.load("vubnguyen", {
+  build: xrun.serial("clean", xrun.exec("touch ../../.env"), xrun.exec("dotenv -e ../../.env -- next build")),
+  clean: () => rimraf.sync(".next"),
+});
