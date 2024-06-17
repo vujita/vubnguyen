e28a05b8a37e7156ec3f9ac5b3988fff60d537da
Vu Nguyen <shardphoneix@gmail.com>
2024-06-17 00:45:13 -0700

    remove DATABASE_URL from being required for build (#246)
    Any questions should be directed to @vujita
    
    ---
    
    Replace any ":question:" below with information about your pull request.
    
    ## Pull Request Details
    
    Provide details about your pull request and what it adds, fixes, or
    changes.
    
    :question:
    
    ## Breaking Changes
    
    Describe what features are broken by this pull request and why, if any.
    
    :question:
    
    ## Issues Fixed
    
    Enter the issue numbers resolved by this pull request below, if any.
    
    1. :question:
    
    ## Other Relevant Information
    
    Provide any other important details below.
    
    :question:

--- apps/vubnguyen/src/env.mjs
+++ apps/vubnguyen/src/env.mjs
@@ -1,5 +1,4 @@
 import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
 
 export const env = createEnv({
   /**
@@ -22,8 +21,6 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app isn't
    * built with invalid env vars.
    */
-  server: {
-    DATABASE_URL: z.string().url(),
-  },
+  server: {},
   skipValidation: !!process.env.CI || !!process.env.SKIP_ENV_VALIDATION,
 });
