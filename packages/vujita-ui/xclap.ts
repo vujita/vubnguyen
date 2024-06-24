6433384188228eb5aaf323dfa2a70a506da01811
Vu Nguyen <shardphoneix@gmail.com>
2024-06-24 08:35:23 -0700

    add vercel.json to ignore gh-pages branch

--- packages/vujita-ui/xclap.ts
+++ packages/vujita-ui/xclap.ts
@@ -1,8 +1,8 @@
 const xrun = require("@xarc/run");
 
 xrun.load({
-  "build:lib": xrun.serial("build:clean", "build:tsup"),
   "build:clean": xrun.exec("rimraf ./*.mjs ./*.js ./*.mts ./*.d.ts ./*.map storybook-static coverage ./*.css"),
+  "build:lib": xrun.serial("build:clean", "build:tsup"),
   "build:tsup": xrun.exec("tsup"),
-  "storybook:build": xrun.exec("storybook build"),
+  "storybook:build": xrun.serial(xrun.exec("storybook build"), xrun.exec("cp files/vercel.json storybook-static/")),
 });
