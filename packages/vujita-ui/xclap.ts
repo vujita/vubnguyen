7e0c4674efe28fa0e28a005a4851f35fb0a7459e
Vu Nguyen <shardphoneix@gmail.com>
2024-06-29 12:43:25 -0700

    setup declaration maps

--- packages/vujita-ui/xclap.ts
+++ packages/vujita-ui/xclap.ts
@@ -2,7 +2,8 @@ const xrun = require("@xarc/run");
 
 xrun.load({
   "build:clean": xrun.exec("rimraf ./*.mjs ./*.js ./*.mts ./*.d.ts ./*.map storybook-static coverage ./*.css dist"),
-  "build:lib": xrun.serial("build:clean", "build:tsup"),
+  "build:declarationMap": xrun.exec("tsc --emitDeclarationOnly -p tsconfig.types.json"),
+  "build:lib": xrun.serial("build:clean", "build:tsup", "build:declarationMap"),
   "build:tsup": xrun.exec("tsup"),
   "storybook:build": xrun.serial(xrun.exec("storybook build"), xrun.exec("cp files/vercel.json storybook-static/")),
 });
