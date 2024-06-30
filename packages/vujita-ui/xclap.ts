0e5f2a210f320477b2c0de5dfd0a157cbe20e98a
Vu Nguyen <shardphoneix@gmail.com>
2024-06-29 21:12:03 -0700

    Chore declarationmaps (#268)
    Any questions should be directed to @vujita
    
    ---
    
    Replace any ":question:" below with information about your pull request.
    
    ## Pull Request Details
    
    Provide details about your pull request and what it adds, fixes, or
    changes.
    
    :question:
    
    ## Breaking Changes
    
    Describe what features are broken by this pull request and why, if any.
    
    :question:
    
    ## Issues Fixed
    
    Enter the issue numbers resolved by this pull request below, if any.
    
    1. :question:
    
    ## Other Relevant Information
    
    Provide any other important details below.
    
    :question:

--- packages/vujita-ui/xclap.ts
+++ packages/vujita-ui/xclap.ts
@@ -1,8 +1,9 @@
 const xrun = require("@xarc/run");
 
 xrun.load({
-  "build:clean": xrun.exec("rimraf ./*.mjs ./*.js ./*.mts ./*.d.ts ./*.map storybook-static coverage ./*.css"),
-  "build:lib": xrun.serial("build:clean", "build:tsup"),
+  "build:clean": xrun.exec("rimraf ./*.mjs ./*.js ./*.mts ./*.d.ts ./*.map storybook-static coverage ./*.css dist"),
+  "build:declarationMap": xrun.exec("tsc --emitDeclarationOnly -p tsconfig.types.json"),
+  "build:lib": xrun.serial("build:clean", "build:tsup", "build:declarationMap"),
   "build:tsup": xrun.exec("tsup"),
   "storybook:build": xrun.serial(xrun.exec("storybook build"), xrun.exec("cp files/vercel.json storybook-static/")),
 });
