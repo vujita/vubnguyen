3032085ea1b4be6efbef2fe907f5eed609cb8781
Vu Nguyen <shardphoneix@gmail.com>
2024-09-15 11:00:18 -0700

    Vercel trigger script (#340)
    Any questions should be directed to @vujita
    
    ---
    
    Replace any ":question:" below with information about your pull request.
    
    ## Pull Request Details
    
    Provide details about your pull request and what it adds, fixes, or
    changes.
    
    :question:
    
    ## Breaking Changes
    
    Describe what features are broken by this pull request and why, if any.
    
    :question:
    
    ## Issues Fixed
    
    Enter the issue numbers resolved by this pull request below, if any.
    
    1. :question:
    
    ## Other Relevant Information
    
    Provide any other important details below.
    
    :question:
    
    ---------
    
    Signed-off-by: Vu Nguyen <shardphoneix@gmail.com>

--- packages/vujita-ui/xclap.ts
+++ packages/vujita-ui/xclap.ts
@@ -9,5 +9,5 @@ xrun.load({
   "build:clean": xrun.exec("rimraf storybook-static coverage dist"),
   "build:lib": xrun.serial(xrun.exec("tsc -p tsconfig.build.json"), xrun.exec("tsc-alias -p tsconfig.build.json"), xrun.exec("tailwind -i src/index.css -o dist/index.css")),
   "build:watch": ["build:lib", xrun.concurrent(xrun.exec("tsc -p tsconfig.build.json --watch"), xrun.exec("tsc-alias -p tsconfig.build.json --watch"))],
-  "storybook:build": xrun.serial("build:lib", xrun.exec("storybook build"), xrun.exec("cp files/vercel.json storybook-static/")),
+  "storybook:build": xrun.serial("build:lib", xrun.exec("storybook build")),
 });
