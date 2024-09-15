3f17d7306effcdeb9903546f198ecf962618e46d
Vu Nguyen <shardphoneix@gmail.com>
2024-09-15 01:18:09 -0700

    convert from tsup -> tsc (#336)
    Any questions should be directed to @vujita
    
    ---
    
    Replace any ":question:" below with information about your pull request.
    
    ## Pull Request Details
    
    Provide details about your pull request and what it adds, fixes, or
    changes.
    
    :question:
    
    ## Breaking Changes
    
    Describe what features are broken by this pull request and why, if any.
    
    :question:
    
    ## Issues Fixed
    
    Enter the issue numbers resolved by this pull request below, if any.
    
    1. :question:
    
    ## Other Relevant Information
    
    Provide any other important details below.
    
    :question:
    
    Signed-off-by: Vu Nguyen <shardphoneix@gmail.com>

--- packages/vujita-ui/.storybook/preview.ts
+++ packages/vujita-ui/.storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from "@storybook/react";
 
-import "../dist/index.css";
+import "../src/index.css";
 
 const preview: Preview = {
   parameters: {
--- packages/vujita-ui/xclap.ts
+++
@@ -1,9 +0,0 @@
-const xrun = require("@xarc/run");
-
-xrun.load({
-  "build:clean": xrun.exec("rimraf ./*.mjs ./*.js ./*.mts ./*.d.ts ./*.map storybook-static coverage ./*.css dist"),
-  "build:declarationMap": xrun.exec("tsc --emitDeclarationOnly -p tsconfig.types.json"),
-  "build:lib": xrun.serial("build:clean", "build:tsup", "build:declarationMap"),
-  "build:tsup": xrun.exec("tsup"),
-  "storybook:build": xrun.serial(xrun.exec("storybook build"), xrun.exec("cp files/vercel.json storybook-static/")),
-});
