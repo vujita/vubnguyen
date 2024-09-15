481d01f5c54cb88eeaa5bd03b0b2967638fe4e07
Vu Nguyen <shardphoneix@gmail.com>
2024-09-15 01:25:05 -0700

    src->dist css
    Signed-off-by: Vu Nguyen <shardphoneix@gmail.com>

--- packages/vujita-ui/xclap.cjs
+++
@@ -1,8 +0,0 @@
-const xrun = require("@xarc/run");
-
-xrun.load({
-  "build:clean": xrun.exec("rimraf storybook-static coverage dist"),
-  "build:lib": xrun.serial("build:clean", xrun.exec("tsc -p tsconfig.build.json"), xrun.exec("tsc-alias -p tsconfig.build.json")),
-  "build:watch": ["build:lib", xrun.concurrent(xrun.exec("tsc -p tsconfig.build.json --watch"), xrun.exec("tsc-alias -p tsconfig.build.json --watch"))],
-  "storybook:build": xrun.serial(xrun.exec("storybook build"), xrun.exec("cp files/vercel.json storybook-static/")),
-});
