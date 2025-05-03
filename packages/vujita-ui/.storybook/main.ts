9739c4583f6b3669c2a3f7204851474585c701e8
Vu Nguyen <shardphoneix@gmail.com>
2025-05-03 07:15:00 -0700

    upgrade storybook

--- packages/vujita-ui/.storybook/main.ts
+++ packages/vujita-ui/.storybook/main.ts
@@ -11,23 +11,26 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)", "../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-onboarding"), getAbsolutePath("@storybook/addon-interactions")],
+  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-onboarding"), getAbsolutePath("@storybook/addon-interactions"), getAbsolutePath("@storybook/addon-webpack5-compiler-swc"), "@chromatic-com/storybook"],
+
   framework: {
     name: getAbsolutePath("@storybook/react-webpack5"),
     options: {
-      builder: {
-        useSWC: true,
-      },
+      builder: {},
     },
   },
-  docs: {
-    autodocs: "tag",
-  },
+
+  docs: {},
+
   webpackFinal: async (config) => {
     // @ts-ignore
     config.resolve.plugins = [new TsconfigPathsPlugin()];
 
     return config;
+  },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
   },
 };
 export default config;
