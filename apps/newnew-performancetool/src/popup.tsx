e87703c7b5e1cf8b075e355d1d4326fb7576348f
Vu Nguyen <shardphoneix@gmail.com>
2025-12-07 23:38:39 -0600

    remove newnew-performance tools
    Signed-off-by: Vu Nguyen <shardphoneix@gmail.com>

--- apps/newnew-performancetool/src/popup.tsx
+++
@@ -1,8 +0,0 @@
-import "newnew-performancetool/src/index.css";
-
-import { createRoot } from "react-dom/client";
-
-const root = document.getElementById("root")!;
-const App = () => <div className="text-bold p-2 text-lg">{"Popup page, Coming soon!!!"}</div>;
-
-createRoot(root).render(<App />);
--- apps/newnew-performancetool/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import { type Config } from "tailwindcss";
-import vujitaUiPreset from "vujita-ui/tailwind.preset";
-
-export default {
-  content: ["./src/**/*.{ts,tsx,css}", "../../packages/vujita-ui/src/**/*.{ts,tsx}"],
-  presets: [vujitaUiPreset],
-} satisfies Config;
